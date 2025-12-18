3e8d2ac6ec77014028fb62369c8df05a0f057ee5
ZhangZiyin <2775274663@qq.com>
2025-12-18 21:23:39 +0800

    线性投影

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ samples = np.array(tmp_samples)
 del tmp_samples
 
 # 超参数：分批次 B（考虑到物理内存 12GB 的限制）
-B = 32
+B = 16
 batches = []
 for i in range(0, len(samples), B):
     batch = samples[i:i+B]
@@ -37,10 +37,16 @@ d_model = 64
 # 原始输入维度
 d_raw = 5
 '''d_raw = raw_data.shape[1]'''
-# Xavier 方法初始化权重，标准差 sqrt(1/5)
-W_emb = np.random.randn(d_raw, d_model)*np.sqrt(1.0/d_raw)
-# 初始化偏置，全零
-b_emb = np.zeros(d_model)
 
-print(W_emb.shape)
-print(b_emb.shape)
+# Xavier 方法初始化权重，标准差 sqrt(1/5)
+W_ref = np.random.randn(d_raw, d_model)*np.sqrt(1.0/d_raw)
+# 初始化偏置，全零
+b_ref = np.zeros(d_model)
+# 内嵌线性投影矩阵
+
+embedded_E = []
+for X in enumerate(batches):
+    # 做线性投影
+    E = X@W_ref+b_ref
+    embedded_E.append(E)
+print(embedded_E[-1].shape)
